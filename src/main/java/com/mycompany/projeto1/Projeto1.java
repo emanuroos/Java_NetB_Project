a7e3054a852bcec1e50f74d6b8baea1f00833a25
emanuroos <115659838+emanuroos@users.noreply.github.com>
2023-04-15 18:28:07 -0300

    Comit Initialized

---
+++ src/main/java/com/mycompany/projeto1/Projeto1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto1;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class Projeto1 {
+
+    public static void main(String[] args) {
+        car VarCar = new car();
+        VarCar.pack();
+        VarCar.setVisible(true);
+    }
+}
